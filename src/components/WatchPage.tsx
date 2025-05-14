9583520d0f55bd84e2ec28b851201cfbc2e00649
NagaTarun <rnagatarun@gmail.com>
2025-05-15 00:40:12 +0530

    add iframe to render the video and get the videoId from useSearchParams and get params for v(video) and pass it to iframe embed link

---
+++ src/components/WatchPage.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../store/appSlice";
+import { useSearchParams } from "react-router-dom";
+
+const WatchPage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+
+  const [searchParams] = useSearchParams();
+
+  return (
+    <div>
+      <iframe
+        width="1000"
+        height="500"
+        src={"https://www.youtube.com/embed/"+searchParams.get("v")}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default WatchPage;
