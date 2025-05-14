8b0faf68be81c9ef7772eed4245d06b98931a674
NagaTarun <rnagatarun@gmail.com>
2025-05-15 00:30:22 +0530

    add appRouter and oulet

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,26 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />
+      }
+    ],
+  },
+]);
 
 function App() {
   {
@@ -22,7 +42,7 @@ function App() {
     <Provider store={store}>
       <>
         <Header />
-        <Body />
+        <RouterProvider router={appRouter} />
       </>
     </Provider>
   );
--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,11 +1,11 @@
-import MainContainer from "./MainContainer"
 import Sidebar from "./Sidebar"
+import { Outlet } from "react-router-dom"
 
 const Body = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <MainContainer />
+      <Outlet />
     </div>
   )
 }
