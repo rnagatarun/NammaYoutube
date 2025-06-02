03215faa5713fafc9d3aad16176e775c5742c1f7
NagaTarun <rnagatarun@gmail.com>
2025-06-03 00:52:13 +0530

    destructure the values from hook and pass them to search bar for getting the search input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,22 +3,35 @@ import HamBurgerIcon from "../assets/HamBurgerIcon.png";
 import YoutubeIconWithText from "../assets/YoutubeIconWithText.png";
 import userIcon from "../assets/userIcon.png";
 import { toggleMenu } from "../store/appSlice";
-
+import { useYoutubeSearchApi } from "../hooks/useYoutubeSearchApi";
 
 const Header = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const toggleHandler = () => {
-    dispatch(toggleMenu() )
-  }
+    dispatch(toggleMenu());
+  };
+  const { searchQuery, setSearchQuery } = useYoutubeSearchApi();
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-1">
-        <img className="h-8" src={HamBurgerIcon} alt="menu" onClick={() => toggleHandler()}/>
+        <img
+          className="h-8"
+          src={HamBurgerIcon}
+          alt="menu"
+          onClick={() => toggleHandler()}
+        />
         <img className="h-8 ml-2" src={YoutubeIconWithText} alt="Youtube" />
       </div>
       <div className="col-span-10 px-10">
-        <input className="border border-gray-400 w-1/2 p-2 rounded-l-full" type="text" />
-        <button className="border border-gray-400 p-2 rounded-r-full px-5    bg-gray-200">🔍</button>
+        <input
+          className="border border-gray-400 w-1/2 p-2 rounded-l-full"
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button className="border border-gray-400 p-2 rounded-r-full px-5    bg-gray-200">
+          🔍
+        </button>
       </div>
       <div className="col-span-1">
         <img className="h-8" src={userIcon} alt="User" />
