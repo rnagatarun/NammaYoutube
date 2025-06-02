30684cf9d7a5ece081bdd5cdff4911bade537abc
NagaTarun <rnagatarun@gmail.com>
2025-06-02 23:36:25 +0530

    update css of videoCard for better UX

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -18,14 +18,15 @@ const VideoCard = ({ snippet, statistics }: VideoInfo) => {
   const { channelTitle, title, thumbnails } = snippet;
 
   return (
-    <div className="p-2 m-2 w-70 shadow-lg">
-      <img className="object-cover w-full h-37" src={thumbnails.high.url} alt="Thumbnail" />
-      <ul>
-        <li className="font-bold py-2">{title}</li>
-        <li>{channelTitle}</li>
-        <li>{statistics.viewCount}</li>
+    <div className="p-2 m-2 w-96 h-96 shadow-lg flex flex-col">
+      <img className="object-cover w-full h-56 rounded-lg" src={thumbnails.high.url} alt="Thumbnail" />
+      <ul className="flex-grow">
+        <li className="font-bold py-2 line-clamp-2 overflow-hidden text-ellipsis">{title}</li>
+        <li className="text-sm text-gray-600">{channelTitle}</li>
+        <li className="text-sm text-gray-600">{statistics.viewCount} views</li>
       </ul>
     </div>
   );
 };
-export default VideoCard;
+
+export default VideoCard; 
