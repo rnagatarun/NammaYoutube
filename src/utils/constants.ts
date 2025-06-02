5f1c464990b1b608016ce01b9226fc128b6b589e
NagaTarun <rnagatarun@gmail.com>
2025-06-02 23:36:58 +0530

    add Youtube search API Constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,2 @@
 export const BUTTON_LIST = ['All', 'Music', 'News','Gen AI', 'Agentic AI', 'Watched', 'Stocks', 'Comedy', 'Telugu Movies', 'Web Series', 'Stock Markets', 'Background Music', 'Rhythms', 'Information Technology'];
+export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
