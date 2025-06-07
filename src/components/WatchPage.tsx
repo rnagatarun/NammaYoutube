5b0b9a20f2d96c32d8331da8a5fc267ccb6a6ad6
NagaTarun <rnagatarun@gmail.com>
2025-06-07 22:25:07 +0530

    render CommentsContainer component on WatchPage

--- src/components/WatchPage.tsx
+++ src/components/WatchPage.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../store/appSlice";
 import { useSearchParams } from "react-router-dom";
-
+import CommentsContainer from "./CommentsContainer";
 const WatchPage = () => {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -12,18 +12,23 @@ const WatchPage = () => {
   const [searchParams] = useSearchParams();
 
   return (
-    <div>
-      <iframe
-        width="1000"
-        height="500"
-        src={"https://www.youtube.com/embed/"+searchParams.get("v")}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
-    </div>
+    <>
+      <div className="flex flex-col">
+        <div>
+          <iframe
+            width="1000"
+            height="500"
+            src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <CommentsContainer />
+      </div>
+    </>
   );
 };
 
