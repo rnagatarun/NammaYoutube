cdc1ea3e8523545a2db3f4bbc84bcf94e81a15e0
NagaTarun <rnagatarun@gmail.com>
2025-06-08 16:34:38 +0530

    update css to match heights of live chat

--- src/components/LiveChat.tsx
+++ src/components/LiveChat.tsx
@@ -30,31 +30,33 @@ const LiveChat = () => {
 
   return (
     <>
-      <div className="ml-2 w-full h-[500px] p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+      <div className="w-full h-[430px] p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
         {liveMessages.map((c, i) => (
           <ChatMessage key={i} name={c.name} message={c.message} />
         ))}
       </div>
 
       <form
-        className="w-full p-4 ml-2 border border-gray-300 bg-white rounded-lg flex items-center gap-3 shadow-sm"
+        className="w-full h-[70px] p-2 border border-gray-300 bg-white rounded-lg flex items-center gap-2 shadow-sm"
         onSubmit={(e) => {
           e.preventDefault();
-          dispatch(addMessage({
-            name: "NagaTarun",
-            message: liveChatMessage
-          }))
-          setLiveChatMessage("")
-         }}
+          dispatch(
+            addMessage({
+              name: "NagaTarun",
+              message: liveChatMessage,
+            })
+          );
+          setLiveChatMessage("");
+        }}
       >
         <input
-          className="flex-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+          className="flex-1 px-2 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
           type="text"
           value={liveChatMessage}
           onChange={(e) => setLiveChatMessage(e.target.value)}
           placeholder="Type your message..."
         />
-        <button className="px-5 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition cursor-pointer">
+        <button className="px-4 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition cursor-pointer">
           Send
         </button>
       </form>
